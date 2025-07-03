aeed4ed71538fd80ad22bff7fae96bfeb8e737e9
yodin327 <daebum2327@gmail.com>
2025-07-02 22:40:25 -0700

    화면 구조 완성

--- app/src/main/java/com/example/save_map/MainActivity.java
+++ app/src/main/java/com/example/save_map/MainActivity.java
@@ -1,34 +1,53 @@
 package com.example.save_map;
 
 import android.os.Bundle;
-import androidx.activity.ComponentActivity;
-import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TextView;
-import android.widget.LinearLayout;
-import android.view.Gravity;
-import android.graphics.Color;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setBackgroundColor(Color.parseColor("#f1f8e9"));
-        layout.setGravity(Gravity.CENTER);
-
-        TextView title = new TextView(this);
-        title.setText("BC Transit Navigator");
-        title.setTextSize(24);
-        title.setTextColor(Color.WHITE);
-        title.setBackgroundColor(Color.parseColor("#8bc34a"));
-        title.setPadding(32, 64, 32, 64);
-        title.setGravity(Gravity.CENTER);
-
-        layout.addView(title);
-
-        setContentView(layout);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_maps:
+                        selectedFragment = new MapsFragment();
+                        break;
+                    case R.id.nav_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.nav_history:
+                        selectedFragment = new HistoryFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+                }
+                return true;
+            }
+        });
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/save_map/SearchFragment.java
@@ -0,0 +1,22 @@
+package com.example.save_map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.widget.TextView;
+
+public class SearchFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView tv = new TextView(getContext());
+        tv.setText("경로 탐색 화면 (SearchFragment)");
+        tv.setTextSize(20);
+        tv.setPadding(32, 64, 32, 64);
+        return tv;
+    }
+}
