916cec3e4fa7d943bb5a8a942577f034a48e7a82
yodin327 <daebum2327@gmail.com>
2025-07-08 20:00:47 -0700

    경로 탐색 화면 뼈대 구현

--- app/src/main/java/com/example/save_map/SearchFragment.java
+++ app/src/main/java/com/example/save_map/SearchFragment.java
@@ -7,16 +7,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.search.SearchBar;
 
 public class SearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView tv = new TextView(getContext());
-        tv.setText("경로 탐색 화면 (SearchFragment)");
-        tv.setTextSize(20);
-        tv.setPadding(32, 64, 32, 64);
-        return tv;
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        SearchBar searchBar = view.findViewById(R.id.search_bar);
+        ChipGroup filterChipGroup = view.findViewById(R.id.filter_chip_group);
+        RecyclerView routeResultList = view.findViewById(R.id.route_result_list);
+        // TODO: 어댑터, 이벤트, 검색/경로 데이터 연동 구현
+        return view;
     }
 }
