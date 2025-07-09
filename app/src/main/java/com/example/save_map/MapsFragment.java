ec83ed38ce1c4df3c5e9f02f62810bd52ea4f681
yodin327 <daebum2327@gmail.com>
2025-07-08 19:59:32 -0700

    UI랑 지도화면 구글 맵 API랑 연동

--- app/src/main/java/com/example/save_map/MapsFragment.java
+++ app/src/main/java/com/example/save_map/MapsFragment.java
@@ -7,16 +7,44 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import android.widget.TextView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView tv = new TextView(getContext());
-        tv.setText("지도 화면 (MapsFragment)");
-        tv.setTextSize(20);
-        tv.setPadding(32, 64, 32, 64);
-        return tv;
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+        FloatingActionButton fabMyLocation = view.findViewById(R.id.fab_my_location);
+        fabMyLocation.setOnClickListener(v -> {
+            if (mMap != null) {
+                // 예시: 밴쿠버로 이동
+                LatLng vancouver = new LatLng(49.2827, -123.1207);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(vancouver, 13f));
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(true);
+        mMap.setBuildingsEnabled(true);
+        LatLng vancouver = new LatLng(49.2827, -123.1207);
+        mMap.addMarker(new MarkerOptions().position(vancouver).title("Vancouver"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vancouver, 12f));
     }
 }
